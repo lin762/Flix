1dd66ed9bce496153703551e2cef68ef59f90e24
Chris Lin <pwnzerness@gmail.com>
2018-01-20 23:47:08 -0500

    added collection view

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Chris lin on 1/20/18.
+//  Copyright © 2018 Chris lin. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var poasterImageView: UIImageView!
+}
